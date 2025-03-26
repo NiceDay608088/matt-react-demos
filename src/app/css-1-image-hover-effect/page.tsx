b43e72a8f8e3ed0e0b518283280c00397abcee65
unknown <abc608088@126.com>
2025-03-26 22:08:18 +0800

    feat ImageHoverEffectPage

---
+++ src/app/css-1-image-hover-effect/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import styles from "./ImageHoverEffectPage.module.css";
+
+const ImageHoverEffectPage = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className={styles.imageContainer}>
+        <Image src="/sports_man_1.png" height={200} width={200} alt="" />
+        <div className={styles.message}>this is the first a</div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/sports_man_2.png" height={200} width={200} alt="" />
+        <div className={styles.message}>bbb</div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/sports_man_3.png" height={200} width={200} alt="" />
+        <div className={styles.message}>ccc</div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/sports_woman_1.png" height={200} width={200} alt="" />
+        <div className={styles.message}>ddd</div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageHoverEffectPage;
